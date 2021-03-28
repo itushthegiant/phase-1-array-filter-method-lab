ddb4b86746d887cc7efe1ca994e1ca78ecc273c4
etamar <etamar.levy@gmail.com>
2021-03-28 19:29:48 -0400

    commit tree 7df41168a873896e2dd42c51fc99c34cf18b3b04

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+function findMatching(arrOfDrivers, driver) {
+    const result = arrOfDrivers.filter(name => name.toLowerCase() === driver.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(arrOfDrivers, driver) {
+    const newArr = arrOfDrivers.filter(name => {
+       return name.startsWith(driver);
+    })
+    return newArr;
+}
+
+function matchName(arrOfDrivers, driver) {
+    const result = arrOfDrivers.filter(obj => {
+        return obj.name === driver;
+    })
+    return result;
+}
